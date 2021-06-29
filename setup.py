3b987e5d79953fc75b38935d9a8a388c085eeb9c
mogaiskii <sani.mog@gmail.com>
2021-06-29 18:57:06 +0300

    upd version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="magic_config",
-    version="0.0.4",
+    version="0.0.5",
     author="mogaiskii",
     author_email="sani.mog@gmail.com",
     description="declarative settings with multiple backends",
